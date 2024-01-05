85c9b188221e5bbe05c73a0ad9457d1518f35b36
Rumerx10 <rumerx10@gmial.com>
2024-01-05 15:14:13 +0600

    small change in header.html

--- auction_project/auction_project/auction_project/settings.py
+++ auction_project/auction_project/auction_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [STATIC_DIR,]
 
 
